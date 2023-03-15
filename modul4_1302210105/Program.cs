e24237df43fcac3e4cfa58a53786ab2cbac5836d
Onesiforus-Rade-Putra <127029013+Onesiforus-Rade-Putra@users.noreply.github.com>
2023-03-15 16:19:29 +0700

    menambahkan class Kodebuah
    commit ke 2

--- modul4_1302210105/Program.cs
+++ modul4_1302210105/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul4_1302210105;
+
+kodebuah kb = new kodebuah();
+Console.WriteLine(kb.getkodebuah(kodebuah.buah.Apel));
---
+++ modul4_1302210105/kodebuah.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210105
+{
+    internal class kodebuah
+    {
+        public enum buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung }
+        String[] arr = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00" };
+
+        public string getkodebuah(buah namaBuah) => arr[(int)namaBuah];
+        
+    }
+}
